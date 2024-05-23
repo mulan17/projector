09dadbc30e85eebe563c21ae3219bd6ce87865d7
Anastasiya Mulyukova <anastasiya@Anastasiyas-MacBook-Air.fritz.box>
2024-05-23 14:45:13 +0200

    new commit to hw3

---
+++ web3/hw3/main.go
@@ -0,0 +1,83 @@
+// Розробити гру-текстовий квест «Новий світ».
+
+// Ваш персонаж прокидається в невідомому місці з деякими речами. Він нічого не памʼятає. 
+// У нього є можливість піти одним з кількох шляхів (усі перелічені сутності — структури). 
+// Ситуація розвивається залежно від обраного рішення.
+
+// Ігровий режим: текстом пишеться ситуація і пропонуються текстові варіанти, які може обрати гравець. 
+// Гравець пише один з варіантів і читає, як у цьому випадку розвивається ситуація.
+
+// Можливий сценарій: Стівен прокинувся біля входу в печеру. Він лише памʼятає своє імʼя. 
+// Поряд з ним рюкзак, в якому він знаходить сірники, ліхтарик і ніж. У печері темно, тому Стівен іде стежкою, 
+// яка веде від печери в ліс. У лісі Стівен натикається на мертве тіло дивної тварини. 
+// Він обирає нічого з цим не робити й іти далі. Через деякий час Стівен приходить до безлюдного табору. 
+// Він вже втомлений і вирішує відпочити, а не йти далі. У найближчому наметі він знаходить сейф з кодовим замком з двох чисел. 
+// Він добирає код, і коли сейф відчиняється, йому на долоню виповзає велика комаха, кусає його й тікає. 
+// Стівен непритомніє. А все могло бути зовсім інакше.
+
+// Треба використати Scan, type struct, if/else, switch/case, for loop.
+
+
+package main
+
+import "fmt"
+
+
+type Person struct {
+	name string
+}
+
+type Bag struct {
+	match bool
+	knife bool
+	ligther bool
+}
+
+type ScanTest struct {
+	text string
+	in   bool
+	out  bool
+}
+
+func (p Person) wakeUp() {
+	fmt.Printf("%v woke ups in a cave. He remember nothing. Help him get home\n", p.name )
+}
+
+func (p Person) goFromCave(){
+	i := true
+	if i == true {
+		fmt.Printf("Great. Now %v will try to find home\n", p.name)
+	}
+}
+
+func (p Person) answear(){
+	var answear string
+	fmt.Println("Do you wanna go out of cave?")
+
+	_, err := fmt.Scan(&answear)
+	if err != nil {
+		fmt.Println("Choose: 'yes' or 'no'")
+		return
+	}
+
+	if answear == "yes" {
+		fmt.Println("Let's go futher")
+	} else {
+		fmt.Println("Game is over")
+	}
+}
+
+func main(){
+	println("Game: New Wordl!")
+
+	p := Person{
+		name: "Stive",
+	}
+
+	p.wakeUp()
+	p.answear()
+	p.goFromCave()
+
+
+
+}
