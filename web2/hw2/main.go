2d8bf2c8c4d1356a6b398beba28f618e7014df6d
Anastasiya Mulyukova <anastasiya@Anastasiyas-MacBook-Air.fritz.box>
2024-05-26 18:16:59 +0200

    updated hw2

--- web2/hw2/main.go
+++ web2/hw2/main.go
@@ -2,68 +2,89 @@
 //Кожну сутність (наглядач, звір, клітка тощо) представляти окремою структурою (zookeeper, animal, cage).
 //Користуємось ембдінгом і методами.
 
+
+// embedding зазвичай використовується, коли треба розширити структуру, в твоєму випадку Cage розширяє тип Animal і це виглядає не зовсім логічним, адже Cage та Animal це сильно різні сутності. 
+//Гарним прикладом було б розширити Animal тип типом Monkey або Lion.
+
+// // Також, виглядає не дуже логічним лічильник тварин в клітці, бо завжди в клітці тільки одна тваринка.
+
+// // Враховуючи коментар з лічильником, я б зробив так, щоб функція CollectAnimal не приймала клітку, а створювала нову, складала в неї тваринку та повертала.
+
+// // PS лічильник можна перенести до зукіпера, тоді буде логічно (шось накшталт скільки цей зукіпер зловив тварин взагалі).
+
 package main
 
 import "fmt"
 
 type Zookeeper struct {
-	Name string
+	Name        string
+	AnimalCount int
 }
 
 type Animal struct {
 	Name string
+	Age  int
+}
+
+type AnimalType struct {
+	Animal
 	Type string
 }
 
-type Cage struct{
-	Animal
-	Contain int
+type Cage struct {
+	Animal AnimalType
 }
 
-func (a Animal) Print(){
+func (a AnimalType) Print() {
 	fmt.Printf("Animal is %v and name is %v\n", a.Type, a.Name)
 }
 
-func (c *Cage) CageContain() {
-	fmt.Printf("Cage contains: %v\n", c.Contain)
-	
+func (c Cage) CageContain() {
+	fmt.Printf("Cage contains: %v the %v\n", c.Animal.Name, c.Animal.Type)
 }
 
-func (z Zookeeper) NameZookeper(){
-	fmt.Printf("Zookeper on duty is %v\n", z.Name)
-} 
-
-func (z Zookeeper) CollectAnimal(a Animal, c *Cage) {
-	c.Animal = a
-	c.Contain++
-	fmt.Printf("%v has collected %v the %v\n", z.Name, a.Name, a.Type)
+func (z Zookeeper) NameZookeeper() {
+	fmt.Printf("Zookeeper on duty is %v\n", z.Name)
 }
 
-func main(){
-	a1 := Animal{Name: "Leo", Type: "Lion"}
-	a2 := Animal{Name: "John", Type: "Tiger"}
-	a3 := Animal{Name: "Kate", Type: "Squirrel"}
-	a4 := Animal{Name: "Po", Type: "Bear"}
-	a5 := Animal{Name: "Anna", Type: "Cat"}
+func (z *Zookeeper) CollectAnimal(t AnimalType) Cage {
+	z.AnimalCount++
+	fmt.Printf("%v has collected %v the %v\n", z.Name, t.Name, t.Type)
+	return Cage{Animal: t}
+}
 
-	c := Cage{Contain: 0}
+func main() {
+	a1 := Animal{Name: "Leo"}
+	a2 := Animal{Name: "Lala"}
+	a3 := Animal{Name: "Sisi"}
+	a4 := Animal{Name: "Po"}
+	a5 := Animal{Name: "Kate"}
+
+	t1 := AnimalType{Animal: a1, Type: "Lion"}
+	t2 := AnimalType{Animal: a2, Type: "Tiger"}
+	t3 := AnimalType{Animal: a3, Type: "Squirrel"}
+	t4 := AnimalType{Animal: a4, Type: "Bear"}
+	t5 := AnimalType{Animal: a5, Type: "Cat"}
+
 	z := Zookeeper{Name: "Christian"}
 
-	fmt.Printf("Cage is empty, %v animal is there. Who is on duty today?\n", c.Contain)
-	z.NameZookeper()
+	fmt.Println("Cages are empty, 0 animals are there. Who is on duty today?")
+	z.NameZookeeper()
 
-	z.CollectAnimal(a1, &c)
-	c.CageContain()
+	c1 := z.CollectAnimal(t1)
+	c1.CageContain()
 
-	z.CollectAnimal(a2, &c)
-	c.CageContain()
+	c2 := z.CollectAnimal(t2)
+	c2.CageContain()
 
-	z.CollectAnimal(a3, &c)
-	c.CageContain()
+	c3 := z.CollectAnimal(t3)
+	c3.CageContain()
 
-	z.CollectAnimal(a4, &c)
-	c.CageContain()
+	c4 := z.CollectAnimal(t4)
+	c4.CageContain()
 
-	z.CollectAnimal(a5, &c)
-	c.CageContain()
+	c5 := z.CollectAnimal(t5)
+	c5.CageContain()
+
+	fmt.Printf("%v has collected a total of %v animals.\n", z.Name, z.AnimalCount)
 }
