257a19b2179ce41e4bed29c63e39294a1c3ae910
Anastasiya <mulyukova.an@gmail.com>
2024-06-07 15:47:33 +0200

    updated hw4

--- web4/example/main.go
+++ web4/example/main.go
@@ -10,7 +10,70 @@ package main
 
 import "fmt"
 
-func main() {
+func main(){
+	var firstSlice = make([]string, 4)
+
+	for i := range firstSlice{
+		firstSlice[i] = "banana"
+	}
+
+	fmt.Println("1. firstSlice", firstSlice)
+
+// 0:len(firstSlice)
+	var secondSlice = firstSlice[:]
+
+	secondSlice[1] = "apple"
+
+	fmt.Println("2. firstSlice", firstSlice)
+	fmt.Println("2. secondSlice", secondSlice)
+
+	secondSlice = secondSlice[1:]
+	fmt.Println("3. firstSlice", firstSlice)
+	fmt.Println("3. secondSlice", secondSlice)
+
+	// // a[low:high:max]
+	// The indices low and high select which elements of operand a appear in the result.
+	// Max controls the resulting slice's capacity by setting it to max - low.
+	// Language specs: https://go.dev/ref/spec#Slice_expressions (Full slice expressions)
+	thirdSlice := firstSlice[2:4:4] 
+	fmt.Println("4. thirdSlice", thirdSlice, cap(thirdSlice))
+
+}
+
+func exampleSlice() {
+
+	var sliceLiteral []int
+
+	sliceLiteral = []int{101, 2020, 303}
+
+	fmt.Println(sliceLiteral, len(sliceLiteral), cap(sliceLiteral))
+
+	//створює масив з 3 елементами, з нього створює слайс, вказуе слайс на початок масиву та засовує в слайс літерал
+	sliceMake := make([]int, 1, 3)
+
+	fmt.Println(sliceMake, len(sliceMake), cap(sliceMake))
+
+	sliceMake[0] = 101
+
+	poinetToFirstElem := &sliceMake[0]
+
+	fmt.Println("By pointer:", *poinetToFirstElem)
+
+	fmt.Println(sliceMake, len(sliceMake), cap(sliceMake))
+
+	sliceMake = append(sliceMake, 202, 303, 404)
+
+	*poinetToFirstElem = 55
+
+	if sliceMake[0] == 55 {
+		sliceMake[0] = 505
+	}
+
+	fmt.Println(sliceMake, len(sliceMake), cap(sliceMake))
+	
+}
+
+func exampleArray() {
 	var arr [3]int //3 is lenght
 
 	arr[0] = 1
@@ -37,7 +100,7 @@ func printArr(arr [3]int) {
 	for i, v := range arr {
 		//ми беремо массив і буде змінюватись елемент масиву який ми отримуемо
 		v = 202 //змінна містить елемент масиву, а чи зміниться елемент масиву якщо ми з
-		arr [i] = 202
+		arr[i] = 202
 		fmt.Println(i, arr[i], v)
 	}
 
---
+++ web4/hw4.1/main.go
@@ -0,0 +1,67 @@
+//1. Пошук для текстового редактора. Створити slice string з текстом, який користувач ввів у текстовий редактор.
+//Написати функцію, яка приймає на вхід рядок для пошуку та знаходить у текстовому редакторі всі рядки, які містять рядок пошуку.
+//Використовуючи цю функцію, додати можливість пошуку тексту в текстовому редакторі та вивести на екран усі відповідні результати.
+//Розширена задача: ініціалізувати текс в редакторі не через код програми, а зчитавши рядки тексту з файлу
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func readFromFile(myfile string) ([]string, error) {
+	file, err := os.Open(myfile)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var slice []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		slice = append(slice, scanner.Text())
+	}
+	return slice, scanner.Err()
+}
+
+func searchLines(slice []string, searchWord string) []string {
+	var results []string
+	for _, word := range slice { // пройтись по кожному рядку слайса
+		if strings.Contains(word, searchWord) {
+			results = append(results, word)
+		}
+	}
+	return results
+}
+
+func main() {
+	myfile := "file1.txt" // open the file
+	slice, err := readFromFile(myfile)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	// defer myfile.Close()
+
+	// slice := []string{
+	// 	"one",
+	// 	"two",
+	// 	"three",
+	// }
+	//fmt.Println(Slice)
+
+	searchWord := "так"
+	results := searchLines(slice, searchWord)
+
+	if len(results) > 0 {
+		fmt.Println("Results of searching:")
+		for _, result := range results {
+			fmt.Println((result))
+		}
+	} else {
+		fmt.Println("Not found")
+	}
+}
