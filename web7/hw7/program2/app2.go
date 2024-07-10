7366267c1fe0973f30b3e42b1e11b79614d313c3
Anastasiya <mulyukova.an@gmail.com>
2024-07-10 08:47:43 +0200

    fixed hw7 app2

--- web7/hw7/program2/app2.go
+++ web7/hw7/program2/app2.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"sync"
 	"time"
 )
 
@@ -10,14 +11,25 @@ func main() {
 	numChannel := make(chan int)
 	resultChannel := make(chan [2]int)
 
-	go numberGenerator(numChannel)
-	go findMinAndMax(numChannel, resultChannel)
+	var s sync.WaitGroup
+	s.Add(2)
+
+	go numberGenerator(&s, numChannel, resultChannel)
+	go findMinAndMax(&s, numChannel, resultChannel)
+
+	go func() {
+		
+		s.Wait()
+		close(resultChannel)
+	}()
 
 	result := <-resultChannel
 	fmt.Printf("Min: %d, Max: %d\n", result[0], result[1])
+
 }
 
-func numberGenerator(numChannel chan<- int) {
+func numberGenerator(s *sync.WaitGroup, numChannel chan<- int, resultChannel <-chan [2]int) {
+	defer s.Done()
 	rng := rand.New(rand.NewSource(time.Now().UnixNano())) // Local random generator
 	for i := 0; i < 10; i++ {
 		numChannel <- rng.Intn(100)
@@ -25,7 +37,8 @@ func numberGenerator(numChannel chan<- int) {
 	close(numChannel)
 }
 
-func findMinAndMax(numChannel <-chan int, resultChannel chan<- [2]int) {
+func findMinAndMax(s *sync.WaitGroup, numChannel <-chan int, resultChannel chan<- [2]int) {
+	defer s.Done()
 	min := int(^uint(0) >> 1)
 	max := -min - 1
 
@@ -39,5 +52,4 @@ func findMinAndMax(numChannel <-chan int, resultChannel chan<- [2]int) {
 	}
 
 	resultChannel <- [2]int{min, max}
-	close(resultChannel)
 }
